1a006239ed50c8ef098b0bdbd814a55b6130f2ba
RobbM1970 <55861625+RobbM1970@users.noreply.github.com>
2019-12-16 11:00:21 -0330

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,8 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Assessment'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('AssessmentHeader'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Client'); // Create a table for 'Book' with default settings
+
 mobileApp.tables.add('Customer'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Menuing'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Note'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('PackageDetail'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('PackageHeader'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('PhotoModels'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('Pics'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Users'); // Create a table for 'Book' with default settings
 mobileApp.views.add('ViewModelAssessmentHeader'); // Create a table for 'Book' with default settings
 
