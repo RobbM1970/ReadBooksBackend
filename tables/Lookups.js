df31ddb72eeff193e85520c1a8b3bc153d27202b
RobbM1970 <55861625+RobbM1970@users.noreply.github.com>
2020-01-01 13:23:57 -0330

    Create Lookups.js

---
+++ tables/Lookups.js
@@ -0,0 +1,32 @@
+
+var table = module.exports = require('azure-mobile-apps').table();
+
+
+
+table.read(function (context) {
+
+
+
+
+
+
+
+    return context.execute();
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
